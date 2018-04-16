0c3c308f0a1a45a19bc1b434040458750599b41b
Artsiom <temasprostor@gmail.com>
2018-04-16 17:34:39 +0300

    workhard)

--- js3.js
+++ js3.js
@@ -12,22 +12,58 @@ var searchResult =[
     footer:{
       country:'США',
       imbd: 8.70,
-      year: 2002
+      year: 2002,
+      cach: "$94 000 000"
+    }
+  },
+  {
+    header:{
+      title:'Властелин колец: Две крепости',
+      url:'page2.html'
+    },
+    details:{
+      poster:'img3.jpg',
+      actors:'Элайджа Вуд, Шон Эстин, Орландо Блум, Вигго Мортенсен, Иэн МакКеллен, Доминик Монахэн, Миранда Отто, Джон Рис-Дэвис, Энди Серкис, Билли Бойд',
+      description: ' Братство распалось, но Кольцо Всевластья должно быть уничтожено. Фродо и Сэм вынуждены доверить свои жизни Голлуму, который взялся провести их к вратам Мордора. Громадная Армия Сарумана приближается: члены братства и их союзники готовы принять бой.'
+    },
+    footer:{
+      country:'США',
+      imbd: 8.70,
+      year: 2002,
+      cach: "$94 000 000"
+    }
+  },
+  {
+    header:{
+      title:'Властелин колец: Две крепости',
+      url:'page2.html'
+    },
+    details:{
+      poster:'img3.jpg',
+      actors:'Элайджа Вуд, Шон Эстин, Орландо Блум, Вигго Мортенсен, Иэн МакКеллен, Доминик Монахэн, Миранда Отто, Джон Рис-Дэвис, Энди Серкис, Билли Бойд',
+      description: ' Братство распалось, но Кольцо Всевластья должно быть уничтожено. Фродо и Сэм вынуждены доверить свои жизни Голлуму, который взялся провести их к вратам Мордора. Громадная Армия Сарумана приближается: члены братства и их союзники готовы принять бой.'
+    },
+    footer:{
+      country:'США',
+      imbd: 8.70,
+      year: 2002,
+      cach: "$94"
     }
   }
+
 ];
 function createFooter(footer){
-  var footer=document.createElement('footer');
+  var footerElement=document.createElement('footer');
 
-  footer.innerHTML = '<footer><p><strong>Год:</strong> <time>'+footer.yer+'</time>,<strong>Оценка<abbr aria-label="Internet Movie Database">IMDb<span aria-hidden="true" class="tooltip">Internet Movie Database</span></abbr>:</strong> '+footer.imdb+' <small>(1 349 172)</small>,<strong>Бюджет:</strong> $94 000 000,<strong>Страна:</strong> США</p></footer>';
-  return footer;
+  footerElement.innerHTML = '<p><strong>Год:</strong> <time>'+footer.year+'</time>,<strong>Оценка&nbsp;<abbr aria-label="Internet Movie Database">IMDb<span aria-hidden="true" class="tooltip">Internet Movie Database</span></abbr>:</strong> '+footer.imdb+' <small>(1 349 172)</small>,<strong>Бюджет:</strong> '+footer.cach+',<strong>Страна:&nbsp;</strong>'+footer.country+'</p>';
+  return footerElement;
 
 }
-//// TODO:
-function createMovieDescription(details){
-details.innerHTML= '<div class = "movie-description"><figure><img src="img1.jpg" /><figcaption>Обложка фильма</figcaption></figure><div><section><h1>Описание</h1><p>Последняя часть трилогии о Кольце Всевластия и о героях, взявших на себя бремя спасения Средиземья.Повелитель сил Тьмы Саурон направляет свои бесчисленные рати под стены Минас-Тирита, крепости Последней Надежды.</p></section><section><h1>Актеры</h1><p>Элайджа Вуд, Вигго Мортенсен, Шон Эстин, Иэн МакКеллен, Орландо Блум, Доминик Монахэн, Билли Бойд, Энди Серкис, Миранда Отто, Бернард Хилл</p></section></div></div>'
 
-  return document.createElement('details');
+function createMovieDescription(details){
+  var detailsElement = document.createElement('div');
+  detailsElement.innerHTML= '<div class = "movie-description"><figure><img src="'+details.poster+'" /><figcaption>Обложка фильма</figcaption></figure><div><section><h1>Описание</h1><p>Последняя часть трилогии о Кольце Всевластия и о героях, взявших на себя бремя спасения Средиземья.Повелитель сил Тьмы Саурон направляет свои бесчисленные рати под стены Минас-Тирита, крепости Последней Надежды.</p></section><section><h1>Актеры</h1><p>'+details.actors+'</p></section></div></div>';
+  return detailsElement;
 }
 
 function createHeader(header){
@@ -48,12 +84,12 @@ function createArticle(film){
   article.appendChild(createHeader(film.header));
   article.appendChild(createMovieDescription(film.details));
   article.appendChild(createFooter(film.footer));
-
   return article;
-
 }
 
-for(var i=0 ;i < searchResult.length;i++){
-  console.log(searchResult[i]);
-  main.appendChild(createArticle(searchResult[i]));
-}
+document.addEventListener('DOMContentLoaded', function(){
+  for(var i=0 ;i < searchResult.length;i++){
+    console.log(searchResult[i]);
+    main.appendChild(createArticle(searchResult[i]));
+  }
+}, false);
